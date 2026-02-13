69c03b16c173c59c57d4f7ca073e5b229b0f5d6f
Benjamin <41596271+xBJx@users.noreply.github.com>
2026-02-12 17:48:15 -0700

    Update interface.py
    updated to values that have been under testing on my 2020 Yukon

--- opendbc/car/gm/interface.py
+++ opendbc/car/gm/interface.py
@@ -216,7 +216,7 @@ class CarInterface(CarInterfaceBase):
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     elif candidate == CAR.GMC_YUKON:
-      ret.steerActuatorDelay = 0.5
+      ret.steerActuatorDelay = 0.2
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
       ret.dashcamOnly = True  # Needs steerRatio, tireStiffness, and lat accel factor tuning
 
